cda3335e301287bc73fb20ffd63ed432b081c373
Sergey Kryukov <cydkom@gmail.com>
2019-01-07 00:34:26 +0300

    add items  to list

--- shopping_app.py
+++ shopping_app.py
@@ -1,3 +1,17 @@
+# Create a new empty list named shopping_list
+
+shopping_list = []
+
+
+
+# Create a function named add_list that declares a parametr named item
+    # Add the item to the list
+def add_list(items):
+    shopping_list.append(items)
+    # Notify user that the item was added, and state the number of items in the list current
+    print("Added! List has {} items.".format(len(shopping_list)))
+
+
 def show_help():
     print("What should we pick up at the store? ")
     print("""
@@ -6,10 +20,13 @@ Enter 'HELP' for this help.
     """)
 show_help()
 while True:
-    new_item = input("> ")
+    new_items = input("> ")
 
-    if new_item == 'DONE':
+    if new_items == 'DONE':
         break
-    elif new_item == 'HELP'
+    elif new_items == 'HELP':
         show_help()
         continue
+
+    # Call add to list with new_item as an argument
+    add_list(new_items)
