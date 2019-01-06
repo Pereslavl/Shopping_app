88fcecbc1201ba08b0db34d12f2426c13a2a7280
Sergey Kryukov <cydkom@gmail.com>
2019-01-07 01:48:24 +0300

    SHOW LIST

--- shopping_app.py
+++ shopping_app.py
@@ -1,15 +1,6 @@
-# Create a new empty list named shopping_list
+
 
 shopping_list = []
-
-
-
-# Create a function named add_list that declares a parametr named item
-    # Add the item to the list
-def add_list(items):
-    shopping_list.append(items)
-    # Notify user that the item was added, and state the number of items in the list current
-    print("Added! List has {} items.".format(len(shopping_list)))
 
 
 def show_help():
@@ -17,16 +8,37 @@ def show_help():
     print("""
 Enter 'DONE' to stop adding items.
 Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
     """)
+
+
+def add_list(items):
+    shopping_list.append(items)
+    print("Added! List has {} items.".format(len(shopping_list)))
+
+
+def show_list():
+    print("Here`s your list:")
+    for item in shopping_list:
+        print(item)
+
 show_help()
 while True:
     new_items = input("> ")
-
     if new_items == 'DONE':
         break
     elif new_items == 'HELP':
         show_help()
         continue
 
-    # Call add to list with new_item as an argument
+
+# Define a function named  show_list that prints all the items in the list
+# HINT: Make sure to run itself.
+    elif new_items == "SHOW":
+        show_list()
+        continue
+
+    #Finable the SHOW command to show  the list. Don`t forget to update  the HELP documentation
     add_list(new_items)
+
+show_list()
